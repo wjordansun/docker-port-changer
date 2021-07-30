63d1da4175d1ceae5a0c3a837787871080beabf2
Jordan Sun <wsun12@stevens.edu>
2021-07-29 20:25:28 -0400

    shell

--- main.go
+++ main.go
@@ -1,53 +1,39 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"log"
-	"portchanger/docker"
+	"os"
+	"os/exec"
+	"portchanger/packet"
 	"strings"
-	"time"
-
-	"github.com/google/gopacket"
-	"github.com/google/gopacket/pcap"
 )
-
-var (
-    device       string = "docker0"
-    snapshot_len int32  = 1024
-    promiscuous  bool   = false
-    err          error
-    timeout      time.Duration = 1 * time.Second
-    handle       *pcap.Handle
-)
-
+func runCommand(commandStr string) error {
+	commandStr = strings.TrimSuffix(commandStr, "\n")
+	arrCommandStr := strings.Fields(commandStr)
+	switch arrCommandStr[0] {
+	case "exit":
+		os.Exit(0)
+	case "quit":
+		os.Exit(0)
+	}
+	cmd := exec.Command(arrCommandStr[0], arrCommandStr[1:]...)
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	return cmd.Run()
+}
 func main() {
-    // Open device
-    handle, err = pcap.OpenLive(device, snapshot_len, promiscuous, timeout)
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer handle.Close()
-
-    // Set filter
-    // var filter string = "tcp and port 8080"
-    // err = handle.SetBPFFilter(filter)
-    // if err != nil {
-    //     log.Fatal(err)
-    // }
-    // fmt.Println("Only capturing TCP port 8080 packets.")
-
-    packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
-    for packet := range packetSource.Packets() {
-				pac := packet.String()
-				//fmt.Println(packet)
-        fmt.Println(strings.Contains(pac, "RST=true"))
-				if strings.Contains(pac, "RST=true") {
-
-						docker.Stop()
-				
-
-            docker.Start()
-        }
-    }
-
+	packet.Listen()
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("Safehouse> ")
+		cmdString, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+		err = runCommand(cmdString)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+	}
 }
---
+++ packet/packet.go
@@ -0,0 +1,54 @@
+package packet
+
+import (
+	"fmt"
+	"log"
+	"portchanger/docker"
+	"strings"
+	"time"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/pcap"
+)
+
+var (
+    device       string = "docker0"
+    snapshot_len int32  = 1024
+    promiscuous  bool   = false
+    err          error
+    timeout      time.Duration = 1 * time.Second
+    handle       *pcap.Handle
+)
+
+func Listen() {
+    // Open device
+    handle, err = pcap.OpenLive(device, snapshot_len, promiscuous, timeout)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer handle.Close()
+
+    // Set filter
+    // var filter string = "tcp and port 8080"
+    // err = handle.SetBPFFilter(filter)
+    // if err != nil {
+    //     log.Fatal(err)
+    // }
+    // fmt.Println("Only capturing TCP port 8080 packets.")
+
+    packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+    for packet := range packetSource.Packets() {
+				pac := packet.String()
+				//fmt.Println(packet)
+        //fmt.Println(strings.Contains(pac, "RST=true"))
+				if strings.Contains(pac, "RST=true") {
+
+					fmt.Println(packet)
+					
+					docker.Stop()
+
+          docker.Start()
+        }
+    }
+
+}
